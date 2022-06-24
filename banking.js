dab6eb8a4077d53ca98e32d4c81c7842339879b2
Zahid-Hasan-Shuvo <shuvo.cse6.bu@gmail.com>
2022-06-24 08:05:28 +0600

    inputTaka show

--- banking.js
+++ banking.js
@@ -1,4 +1,10 @@
 document.getElementById('deposit-button').addEventListener('click', function(){
-
+ const depositInput=document.getElementById('deposit-input');
+  depositValue=depositInput.value;
+ 
+ const depositTaka=document.getElementById('deposit-taka');
+ depositTaka.innerText=depositValue;
+  depositInput.value='';
+ 
 
 })
